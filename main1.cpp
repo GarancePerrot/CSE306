5177bef5db126136dfe2533075ec734e7ed0abdd
GarancePerrot <115420755+GarancePerrot@users.noreply.github.com>
2024-04-24 17:23:34 +0200

    Update main1.cpp
    added antialiasing

--- main1.cpp
+++ main1.cpp
@@ -15,7 +15,7 @@
 
 int main() {
 	clock_t start = clock(); //chronometer for execution time
-	
+
 	int W = 512; //width of the screen
 	int H = 512; //height of the screen
 
@@ -26,27 +26,41 @@ int main() {
 	s.I = 2E11; //light intensity
 	Vector C(0,0,55); //camera
 
+	int NB_PATHS = 1000; 
+
 	s.addSphere(Sphere(Vector(0.,1000.,0.),   940.,  Vector(1,0.,0.))); //red ceiling 
-        s.addSphere(Sphere(Vector(0.,-1000.,0.),  940., Vector(0,1,0.))); //blue back wall
-        s.addSphere(Sphere(Vector(0.,0., -1000.), 940.,  Vector(0,0.,1))); //green floor
-        s.addSphere(Sphere(Vector(0.,0.,1000.),   940.,  Vector(0.8,0.,0.2))); //pink front wall
-        s.addSphere(Sphere(Vector(0.,-20.,0.),  20,  Vector(0.1,0.1,0.1), false, false)); //center sphere : isMirror, isTransparent
+    s.addSphere(Sphere(Vector(0.,-1000.,0.),  940., Vector(0,1,0.))); //blue back wall
+    s.addSphere(Sphere(Vector(0.,0., -1000.), 940.,  Vector(0,0.,1))); //green floor
+    s.addSphere(Sphere(Vector(0.,0.,1000.),   940.,  Vector(0.8,0.,0.2))); //pink front wall
+    s.addSphere(Sphere(Vector(0.,-25.,-15.),  25,  Vector(0.1,0.1,0.1), false, false)); //center sphere : isMirror, isTransparent
+	//s.addSphere(Sphere(Vector(-50.,0.,0.), 5, Vector(0., 1, 0.)));
+	//s.addSphere(Sphere(Vector(50.,0.,0.), 20, Vector(0., 0, 1.)));
 
-
+	double std_dev = 0.5;
 	double z = -W/(2*tan(alpha/2)); 
 	
 	std::vector<unsigned char> image(W * H * 3, 0);
 	#pragma omp parallel for schedule(dynamic, 1)
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			double x = j-W/2+0.5;
-			double y = H/2-i-0.5;
-			Vector u(x, y, z); //coordinate of pixel (i,j) in the scene
-			u.normalize();
-			Ray r(C, u); //from the coordinate of each pixel and the camera center, we compute a normalized ray direction
-			int ray_depth = 5;  // controls the number of recursive calls allowed in getColor for refractions / reflections
-			Vector color = s.getColor(r, ray_depth); 
-			
+			Vector pixel_color(0.,0.,0.); 
+
+			for (int k = 0 ; k< NB_PATHS; k++){ //antialiasing 
+				double x_mul, y_mul; 
+				boxMuller(std_dev, x_mul, y_mul);
+				Vector u_mul(x_mul,y_mul,0.);  // contribution from boxMuller
+
+				double x = j-W/2+0.5;  
+				double y = H/2-i-0.5;
+				Vector u(x, y, z); // coordinate of pixel (i,j) in the scene
+
+				u += u_mul; //combining boxMuller and usual 
+
+				u.normalize();
+				Ray r(C, u); //from the coordinate of each pixel and the camera center, we compute a normalized ray direction
+				int ray_depth = 5;  // controls the number of recursive calls allowed in getColor for refractions / reflections
+				pixel_color += s.getColor(r, ray_depth); 
+			}			
 			//without gamma correction : 
 			// image[(i * W + j) * 3 + 0] = color[0];
 			// image[(i * W + j) * 3 + 1] = color[1];
