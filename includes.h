1468ce77b221858e96a24b04aadd58deb2d24b57
GarancePerrot <115420755+GarancePerrot@users.noreply.github.com>
2024-04-24 10:25:31 +0200

    Update includes.h

--- includes.h
+++ includes.h
@@ -7,6 +7,11 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <random>
+#include <ctime>
 
+
+static std::default_random_engine engine(10) ; 
+static std::uniform_real_distribution<double> uniform(0,1) ;
 
 #endif 
