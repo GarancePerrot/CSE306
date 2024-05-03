ab65cf739efb9acc2a2f187fdbe6434df3f6c096
GarancePerrot <115420755+GarancePerrot@users.noreply.github.com>
2024-05-03 17:01:37 +0200

    project1 in file
    labs 1 and 2

---
+++ project1/includes.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDES_H  
+#define INCLUDES_H
+
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+#include <cstring>
+
+
+static std::default_random_engine engine(10) ; 
+static std::uniform_real_distribution<double> uniform(0,1) ;
+#endif 
