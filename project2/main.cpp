b328089bd03c6d712ee16b7ce37dd323d39495f5
GarancePerrot <115420755+GarancePerrot@users.noreply.github.com>
2024-05-18 16:36:39 +0200

    Update main.cpp
    added a function to display a simple heart and put everything to draw a voronoi diagram in a separate function

--- project2/main.cpp
+++ project2/main.cpp
@@ -2,31 +2,47 @@
 #include "includes.h"
 #include "lab6.cpp"
 
+void draw_heart(){
+    std::vector<Polygon> test;
+    Polygon pol1;
+    pol1.vertices.push_back(Vector(0.5, 0.2, 0)); 
+    pol1.vertices.push_back(Vector(0.3, 0.4, 0));  
+    pol1.vertices.push_back(Vector(0.15, 0.55, 0));  
+    pol1.vertices.push_back(Vector(0.1, 0.7, 0));  
+    pol1.vertices.push_back(Vector(0.2, 0.85, 0));  
+    pol1.vertices.push_back(Vector(0.35, 0.85, 0));  
+    pol1.vertices.push_back(Vector(0.5, 0.8, 0));  
+    pol1.vertices.push_back(Vector(0.65, 0.85, 0));  
+    pol1.vertices.push_back(Vector(0.8, 0.85, 0));  
+    pol1.vertices.push_back(Vector(0.9, 0.7, 0));  
+    pol1.vertices.push_back(Vector(0.85, 0.55, 0));  
+    pol1.vertices.push_back(Vector(0.7, 0.4, 0));  
+    pol1.vertices.push_back(Vector(0.5, 0.2, 0)); 
+    test.push_back(pol1);
+    save_svg(test, "heart.svg"); 
+}
 
-int main(){
-    clock_t start = clock(); //chronometer for execution time
 
-    // std::vector<Polygon> test; 
-    // Polygon pol1, pol2; 
-    // pol1.vertices.push_back(Vector(0,0,0));
-    // pol1.vertices.push_back(Vector(0,1,0));
-    // pol1.vertices.push_back(Vector(1,1,0));
-    // pol2.vertices.push_back(Vector(0.5,0.5,0));
-    // pol2.vertices.push_back(Vector(0.5,1,0));
-    // pol2.vertices.push_back(Vector(1,0.3,0));
-    // pol2.vertices.push_back(Vector(0.3,0.3,0));
-    // test.push_back(pol1);
-    // test.push_back(pol2);
-
+void draw_voronoi(int N){
     Voronoi vor; 
-    int N = 2000; 
     vor.points.resize(N);
-
     for (int i= 0 ; i<N; i++ ){
         vor.points[i] = Vector(rand()/ double(RAND_MAX),rand()/ double(RAND_MAX), 0 );
     }
     vor.compute();
-    save_svg(vor.cells, "test.svg");
+    save_svg(vor.cells, "Voronoi_cells.svg");
+
+}
+
+int main(){
+    clock_t start = clock(); //chronometer for execution time
+
+    // //display a simple polygon in heart shape : 
+    //draw_heart();
+
+    // display the Voronoi cells of a random set of N points: 
+    draw_voronoi(2000); 
+
 
     clock_t end = clock();
 	double execution_time = ((double)end - start) / CLOCKS_PER_SEC;
